0b6930a2e216528b46fff9c9aa72c0f94209059b
wuilder <32497112+wuilder@users.noreply.github.com>
2021-01-22 15:14:16 -0300

    modificando el home

--- src/components/home.js
+++ src/components/home.js
@@ -4,68 +4,87 @@ export default class Footer extends Component {
 
   render(){
     return (
-      <section>
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <section>
-                <div className="container">
-                  <div className="row">
-                    <div className="col"><h4>Command</h4></div>
-                    <div className="col"><h4>Argumets</h4></div>
-                    <div className="col"><h4>Description</h4></div>
-                  </div>
-                </div>
-              </section>
-              <section>
-                <div className="container">
-                  <div className="row">
-                    <div className="col">Trial</div>
-                    <div className="col">-</div>
-                    <div className="col">Create a roster from a embed message when react</div>
-                  </div>
-                </div>
-              </section>
-              <section>
-                <div className="container">
-                  <div className="row">
-                    <div className="col">Raid</div>
-                    <div className="col">Trial  Date Time</div>
-                    <div className="col">Creates a message raid from a roster.</div>
-                  </div>
-                </div>
-              </section>
-              <section>
-                <div className="container">
-                  <div className="row">
-                    <div className="col">Raid</div>
-                    <div className="col">Trial  Date Time</div>
-                    <div className="col">Creates a message raid from a roster.</div>
-                  </div>
-                </div>
-              </section>
-              <section>
-                <div className="container">
-                  <div className="row">
-                    <div className="col">Raid</div>
-                    <div className="col">Trial  Date Time</div>
-                    <div className="col">Creates a message raid from a roster.</div>
-                  </div>
-                </div>
-              </section>
-              <section>
-                <div className="container">
-                  <div className="row">
-                    <div className="col">Raid</div>
-                    <div className="col">Trial  Date Time</div>
-                    <div className="col">Creates a message raid from a roster.</div>
-                  </div>
-                </div>
-              </section>
+      <React.Fragment>
+        <section>
+          <div className="conainer">
+            <div className="row">
+              Introduccion
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+        <section>
+          <div className="conainer">
+            <div className="row">
+              <h4>Command for Users</h4>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="container">
+            <div className="row">
+              <h4>Commands for Admins</h4>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <section>
+                  <div className="container">
+                    <div className="row">
+                      <div className="col"><h4>Command</h4></div>
+                      <div className="col"><h4>Argumets</h4></div>
+                      <div className="col"><h4>Description</h4></div>
+                    </div>
+                  </div>
+                </section>
+                <section>
+                  <div className="container">
+                    <div className="row">
+                      <div className="col">Trial</div>
+                      <div className="col">-</div>
+                      <div className="col">Create a roster from a embed message when react</div>
+                    </div>
+                  </div>
+                </section>
+                <section>
+                  <div className="container">
+                    <div className="row">
+                      <div className="col">Raid</div>
+                      <div className="col">Trial  Date Time</div>
+                      <div className="col">Creates a message raid from a roster.</div>
+                    </div>
+                  </div>
+                </section>
+                <section>
+                  <div className="container">
+                    <div className="row">
+                      <div className="col">Raid</div>
+                      <div className="col">Trial  Date Time</div>
+                      <div className="col">Creates a message raid from a roster.</div>
+                    </div>
+                  </div>
+                </section>
+                <section>
+                  <div className="container">
+                    <div className="row">
+                      <div className="col">Raid</div>
+                      <div className="col">Trial  Date Time</div>
+                      <div className="col">Creates a message raid from a roster.</div>
+                    </div>
+                  </div>
+                </section>
+                <section>
+                  <div className="container">
+                    <div className="row">
+                      <div className="col">Raid</div>
+                      <div className="col">Trial  Date Time</div>
+                      <div className="col">Creates a message raid from a roster.</div>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+        </section>
+      </React.Fragment>
     )
   }
 }
